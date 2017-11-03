08628bbb443457da42c30b709b896da3c8145f8d
Rubens Figueiredo <rubens.figueiredo@bisdn.de>
2017-11-03 13:45:33 +0100

    adding some stuff

--- main.py
+++ main.py
@@ -4,18 +4,28 @@ import services_definition_pb2
 import grpc
 import time
 from prometheus_handler import Prometheus as Prom
+import traceback
 
 #Define data structure to utilize the statistics, and create a class to parse to prometheus
 class Connection:
     
     def __init__(self):
-        self.address = '192.168.121.183:5000'
-        self.stub = sgrpc.NetworkStatisticsStub(grpc.insecure_channel(self.address))
+        self.address = '192.168.121.7:5000'
+
+        self.stubStats = sgrpc.NetworkStatisticsStub(grpc.insecure_channel(self.address))
+        self.stubTop = sgrpc.NetworkDescriptorStub(grpc.insecure_channel(self.address))
+
         self.prom = Prom()
+
+    def __getState(self):
+        try:
+            return self.stubTop.GetTopology(services_definition_pb2.Empty())
+        except BaseException:
+            print "Unexpected error:", traceback.format_exc()
 
     def __getStats(self):
         try:
-            return self.stub.GetStatistics(services_definition_pb2.Empty())
+            return self.stubStats.GetStatistics(services_definition_pb2.Empty())
         except BaseException:
             print "Unexpected error:", traceback.format_exc()
     
@@ -24,7 +34,10 @@ class Connection:
         while True:
             for obj in self.__getStats():
                 # obj.state.counters.in_broadcast_pkts
-                self.prom.addToList(obj)
+                self.prom.addToStatsList(obj)
+
+            self.prom.addToTopoList(self.__getState())
+
             time.sleep(1)
 
 if __name__ == "__main__":
--- prometheus_handler.py
+++ prometheus_handler.py
@@ -1,12 +1,15 @@
 import prometheus_client as pc
 
 class Prometheus:
-    structure = {'ibp' : pc.Gauge('in_broadcast_packets', 'InBroadcastPackets', ['nodeid', 'name']),
+    structureStats = {'ibp' : pc.Gauge('in_broadcast_packets', 'InBroadcastPackets', ['nodeid', 'name']),
                  'obp' : pc.Gauge('out_broadcast_packets', 'OutBroadcastPackets', ['nodeid', 'name']),
                  'ioct' : pc.Gauge('in_octets', 'inOctets', ['nodeid','name']),
                  'idis' : pc.Gauge('in_discards', 'InDiscards', ['nodeid','name']),
                  'odis' : pc.Gauge('out_discards', 'OutDiscards', ['nodeid','name'])                 
                 }
+    structureTop = { 'nlinks' : pc.Gauge('number_of_links', 'NumberLinks'),
+                     'nnodes' : pc.Gauge('number_of_nodes', 'NumberNodes')            
+                    }
 
     def __init__(self):
         self.inBroadPack = 0
@@ -16,10 +19,14 @@ class Prometheus:
     def run(self):
         pc.start_http_server(8001)
 
-    def addToList(self, obj):
+    def addToStatsList(self, obj):
         state = obj.state 
-        self.structure['ibp'].labels(nodeid=obj.name, name=state.name).set(state.counters.in_broadcast_pkts)
-        self.structure['obp'].labels(nodeid=obj.name, name=state.name).set(state.counters.out_broadcast_pkts)
-        self.structure['idis'].labels(nodeid=obj.name, name=state.name).set(state.counters.in_discards)
-        self.structure['odis'].labels(nodeid=obj.name, name=state.name).set(state.counters.out_discards)
-        self.structure['ioct'].labels(nodeid=obj.name, name=state.name).set(state.counters.in_octets)
+        self.structureStats['ibp'].labels(nodeid=obj.name, name=state.name).set(state.counters.in_broadcast_pkts)
+        self.structureStats['obp'].labels(nodeid=obj.name, name=state.name).set(state.counters.out_broadcast_pkts)
+        self.structureStats['idis'].labels(nodeid=obj.name, name=state.name).set(state.counters.in_discards)
+        self.structureStats['odis'].labels(nodeid=obj.name, name=state.name).set(state.counters.out_discards)
+        self.structureStats['ioct'].labels(nodeid=obj.name, name=state.name).set(state.counters.in_octets)
+
+    def addToTopoList(self, obj):
+        self.structureTop['nlinks'].set(len(obj.network[0].link))
+        self.structureTop['nnodes'].set(len(obj.network[0].node))
